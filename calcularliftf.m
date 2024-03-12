16c2883bd0c389b8c177a7d5b9062919e20112c3
David Ortiz Armiñana <56995448+dav-ctrl@users.noreply.github.com>
2024-03-12 09:42:38 +0100

    Update calcularliftf.m

--- calcularliftf.m
+++ calcularliftf.m
@@ -1,5 +1,5 @@
 function liftf=calcularliftf(a)
-    S=["naca 4412","gdes","flap",0.8,999,0.5,a,"eXec"," ","oper","v 110e6","M 0.8",...
+    S=["naca 4412","gdes","flap",0.8,999,0.5,a,"eXec"," ","oper","v 110e6",...
         "seqp","pacc","resultados.txt"," ","a 0","a 0","a 0","a 0","a 0"]'
     writematrix(S,'D:\XFOIL6.99\comandamentsf.txt');
     if exist('resultados.txt', 'file')==2
