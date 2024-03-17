e1e9dec96521caf8a7f62f8edea5c9736f0d1022
David Ortiz Armiñana <56995448+dav-ctrl@users.noreply.github.com>
2024-03-17 21:24:12 +0100

    Update and rename calculardragf.m to dragflap.m

---
+++ dragflap.m
@@ -0,0 +1,17 @@
+function dragf=dragflap(a)
+    Z=importdata('naca4412.txt');
+    S=rotacio(10,Z);
+    writematrix(S,'D:\XFOIL6.99\nacaROT.txt','Delimiter',' ');
+    V=["load nacaROT.txt","NACA","gdes","flap",0.7,999,0.5,a,"eXec"," ","oper","v 3e6",...
+        "seqp","pacc","resultados.txt"," ","a 0","a 0","a 0","a 0","a 0",...
+        "a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0"]';
+    writematrix(V,'D:\XFOIL6.99\comandamentsf.txt');
+    if exist('resultados.txt', 'file')==2
+        delete('resultados.txt');
+    end
+    ! xfoil < comandamentsf.txt
+    t=readtable('resultados.txt');
+    res=table2array(t);
+    a
+    dragf=res(1,3)
+end
