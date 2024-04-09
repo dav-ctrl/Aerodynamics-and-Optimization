d233b54dc47f22e5c3f91c0d1213617cd6fc86ef
David Ortiz Armiñana <56995448+dav-ctrl@users.noreply.github.com>
2024-04-09 10:48:10 +0200

    Update dragflap.m

--- dragflap.m
+++ dragflap.m
@@ -1,8 +1,8 @@
 function dragf=dragflap(a)
     Z=importdata('naca4412.txt');
-    S=rotacio(10,Z);
+    S=rotacio(5,Z);
     writematrix(S,'D:\XFOIL6.99\nacaROT.txt','Delimiter',' ');
-    V=["load nacaROT.txt","NACA","gdes","flap",0.7,999,0.5,a,"eXec"," ","oper","v 3e6",...
+    V=["load nacaROT.txt","NACA","gdes","flap",0.65,999,0.5,a,"eXec"," ","oper","v 3e6",...
         "seqp","pacc","resultados.txt"," ","a 0","a 0","a 0","a 0","a 0",...
         "a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0","a 0"]';
     writematrix(V,'D:\XFOIL6.99\comandamentsf.txt');
